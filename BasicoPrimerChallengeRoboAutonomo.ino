c821172b718d3bcbc14040e858e4c9c973baf769
gleonmen <gleon@buzzpoints.com>
2019-11-25 15:51:06 -0500

    Adding two IR Sensor Logic

--- BasicoPrimerChallengeRoboAutonomo.ino
+++ BasicoPrimerChallengeRoboAutonomo.ino
@@ -23,13 +23,31 @@ const int IN2=6;     // define pin for PWM used to control rotational speed of m
 const int IN3=4;     // define pin used to control rotational direction of motor B  or dirBPin
 const int IN4=5;    // define pin for PWM used to control rotational speed of motor B or pwmBPin
 
-#define IR_sensor_front A0 // front sensor
-#define IR_sensor_back A1 // rear senson
+const int WHITE=0;
+const int BLACK=1;
+
+#define IR_LEFT_FRONT 9     
+#define IR_RIGHT_FRONT 8
+#define IR_RIGHT_BACK 10    
+#define IR_LEFT_BACK 11
+
+
 int distance ;
+int IR_left_front;
+int IR_right_front;
+int IR_left_back;
+int IR_right_back;
+
 
 void setup() {
    Serial.begin(9600);
    dirServo.attach(dirServoPin);  // attaches the servo on servoDirPin to the servo object
+   //IR SENSOR INITIALIZATION
+   pinMode(IR_LEFT_FRONT, INPUT);   
+   pinMode(IR_RIGHT_FRONT, INPUT);  
+   pinMode(IR_RIGHT_BACK, INPUT);   
+   pinMode(IR_LEFT_BACK, INPUT); 
+   
    pinMode(IN1, OUTPUT);   // set dirAPin to output mode
    pinMode(IN2, OUTPUT);   // set pwmAPin to output mode
    pinMode(IN3, OUTPUT);   // set dirBPin to output mode
@@ -38,45 +56,64 @@ void setup() {
 }
 
 void loop(){
-    int IR_front = analogRead(IR_sensor_front);
-    int IR_back = analogRead(IR_sensor_back);
-    distance = ultrasonic.read();
-    ROTATE(200); // start rotete if (distance < 10){
-    Stop();
-    dirServoDegree = 90;
+     read_IR_sensor_values();
+     distance = ultrasonic.read();
+     ROTATE(200); // start rotete if (distance < 10){
+     Stop();
+     dirServoDegree = 90;
     
     while (distance < 10 ) {
         FORWARD(255); 
         distance = ultrasonic.read();
-        IR_front = analogRead(IR_sensor_front);
-        IR_back = analogRead(IR_sensor_back);
-        if ( IR_front > 650 || IR_back > 650 ) { break;}
-          delay(10);
+        read_IR_sensor_values();
+        if ((IR_left_front==BLACK && IR_right_front ==BLACK) || (IR_left_back==BLACK && IR_right_back==BLACK) ){
+           break;
+        }
+        delay(10);
     }
-    //< 650 means white line
-    if (IR_front < 650 ) {  
+    
+    if (IR_left_front==WHITE || IR_right_front == WHITE){
         Stop();
         delay (50);
         BACKWARD(255);
         delay (500);
-    } 
-    if (IR_back < 650 ){
-        Stop();
-        delay (50);
-        FORWARD(255);
-        delay (500);
+    }
+
+    if (IR_left_back==WHITE || IR_right_back == WHITE){
+       Stop();
+       delay (50);
+       FORWARD(255);
+       delay (500);
     }
     // ----------- debugging ----------------
-    DEBUG_CODE(IR_front, IR_back, distance, dirServoDegree);
+    DEBUG_CODE( IR_left_front,  IR_right_front,  IR_left_back,  IR_right_back , distance, dirServoDegree);
 } 
 
-void DEBUG_CODE( int IR_front, int IR_back, int distance, int dirServoDegree ){
+
+void read_IR_sensor_values()
+{
+  IR_left_front = digitalRead(IR_LEFT_FRONT);
+  IR_right_front = digitalRead(IR_RIGHT_FRONT);
+  IR_left_back = digitalRead(IR_LEFT_BACK);
+  IR_right_back = digitalRead(IR_RIGHT_BACK);
+}
+
+void DEBUG_CODE( int IR_left_front, int IR_right_front, int IR_left_back, int IR_right_back , int distance, int dirServoDegree ){
+    Serial.print("distance : " );
     Serial.print(distance);
-    Serial.println("cm");
-    Serial.println("IR front :");
-    Serial.println(IR_front); 
-    Serial.println("IR back :");
-    Serial.println(IR_back);
+    Serial.println("IR_left_front :");
+    Serial.println(IR_left_front); 
+
+    Serial.println("IR_right_front :");
+    Serial.println(IR_right_front); 
+
+    Serial.println("IR_left_back :");
+    Serial.println(IR_left_back); 
+
+    Serial.println("IR_right_back :");
+    Serial.println(IR_right_back); 
+
+    Serial.print("servo Distance : " ); 
     Serial.println(dirServoDegree);
 }
 
